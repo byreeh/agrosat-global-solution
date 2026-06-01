1bcd210806fa880bb43b0ce4c7568d1a65f05612
RM568510 <byreeh1999@gmail.com>
2026-06-01 16:25:29 -0300

    feat: integração API Render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001'
+const BASE_URL = 'https://agrosat-api.onrender.com'
 
 export async function getPlantacoes() {
   const response = await fetch(`${BASE_URL}/plantacoes`)
